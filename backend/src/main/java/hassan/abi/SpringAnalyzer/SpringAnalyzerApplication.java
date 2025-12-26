688e6f1f998e540c6e6a06812a014e387816268f
Hassan ABIDAR <abidar069716@gmail.com>
2025-12-26 14:00:28 +0100

    first commit

---
+++ backend/src/main/java/hassan/abi/SpringAnalyzer/SpringAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package hassan.abi.SpringAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAnalyzerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/hassan/abi/SpringAnalyzer/SpringAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package hassan.abi.SpringAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
